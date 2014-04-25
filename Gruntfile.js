ace69a9d4f9bebf835da4aa9ef2a0efeda741bb4
Karan Goel <karan@goel.im>
2014-04-25 08:58:42 -0700

    add basic grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+
+  // ===========================================================================
+  // CONFIGURE GRUNT ===========================================================
+  // ===========================================================================
+  grunt.initConfig({
+
+    // get the configuration info from package.json ----------------------------
+    // this way we can use things like name and version (pkg.name)
+    pkg: grunt.file.readJSON('package.json'),
+
+    // all of our configuration will go here
+
+    // configure jshint to validate js files -----------------------------------
+    jshint: {
+        options: {
+          reporter: require('jshint-stylish') // use jshint-stylish to make our errors look and read good
+        },
+
+      // when this task is run, lint the Gruntfile and all js files in src
+        build: ['Grunfile.js', 'js/app.js']
+      }
+
+  });
+
+  // ===========================================================================
+  // LOAD GRUNT PLUGINS ========================================================
+  // ===========================================================================
+  // we can only load these if they are in our package.json
+  // make sure you have run npm install so our app can find these
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+};
